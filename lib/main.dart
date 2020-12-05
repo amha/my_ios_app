ca72d7f59faa7ab79dca0cee226ef792b51eaa90
Amha Mogus <amha.mogus@gmail.com>
2020-12-05 21:17:55 +0200

    Initial commit. Created Apple Music playlist UI and action sheet.

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:my_ios_app/views/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'iOS Demo',
+      home: App(),
+    );
+  }
+}
