9fa2ecfb63e2f74e511ef8bf46264d7aae64ac06
Amha Mogus <amha.mogus@gmail.com>
2020-12-12 21:22:34 +0200

    Updated primary CTA on wallet_home.dart and replaced the Cupertino navigation bar added custom header.

--- lib/views/wallet/wallet_home.dart
+++ lib/views/wallet/wallet_home.dart
@@ -13,33 +13,97 @@ class _WalletHomeState extends State<WalletHome> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      backgroundColor: CupertinoColors.systemGrey5,
-      navigationBar: CupertinoNavigationBar(
-        backgroundColor: CupertinoColors.systemGrey5,
-        leading: Text(
-          'Wallet',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-          textAlign: TextAlign.center,
-        ),
-        trailing: CupertinoButton(
-          onPressed: () {},
-          child: Icon(
-            CupertinoIcons.add_circled_solid,
-            color: CupertinoColors.black,
-          ),
-        ),
-      ),
+      backgroundColor: CupertinoColors.systemGrey6,
       child: SingleChildScrollView(
         child: Column(
           children: [
             Container(
-              height: 400,
-              margin: EdgeInsets.all(16),
+              height: 80,
+              margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
               width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.all(16),
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Wallet',
+                    style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: -2),
+                  ),
+                  CupertinoButton(
+                    onPressed: () {},
+                    child: Icon(
+                      CupertinoIcons.add_circled_solid,
+                      color: CupertinoColors.black,
+                      size: 36,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 400,
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              width: MediaQuery.of(context).size.width,
+              clipBehavior: Clip.values[3],
               decoration: BoxDecoration(
-                  color: CupertinoColors.systemGrey2,
+                  color: CupertinoColors.systemGrey5,
                   borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(24, 24, 24, 0),
+                      color: Colors.white,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Text('WHAT\'S NEW'),
+                          ),
+                          Text(
+                            'Now send money to the whole family.',
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.w700),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              'assets/wallet/money.png',
+                              width: 320,
+                              fit: BoxFit.fill,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      color: CupertinoColors.systemGrey5,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Lean more about cash'),
+                          Text(
+                            'START NOW',
+                            style: TextStyle(
+                                color: CupertinoColors.activeBlue,
+                                fontWeight: FontWeight.w600),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
             GestureDetector(
               onTap: () {
